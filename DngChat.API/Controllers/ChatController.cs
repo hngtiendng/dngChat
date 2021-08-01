24e3948801c12e8e17fef9fa286458db97c114f6
hngtiendng <hngtiendng@gmail.com>
2021-08-01 12:47:51 +0700

    update for using another API Address

---
+++ DngChat.API/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using DngChat.API.Hubs;
+using DngChat.API.Hubs.Clients;
+using DngChat.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DngChat.API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
+
+        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        public async Task Post(ChatMessage message)
+        {
+            // run some logic...
+
+            await _chatHub.Clients.All.ReceiveMessage(message);
+        }
+    }
+}
--- DngChat.API/Hubs/ChatHub.cs
+++ DngChat.API/Hubs/ChatHub.cs
@@ -7,10 +7,10 @@ namespace DngChat.API.Hubs
 {
     public class ChatHub : Hub<IChatClient>
     {
-        public async Task SendMessage(ChatMessage message)
-        {
-            //await Clients.All.SendAsync("ReceiveMessage", message);
-            await Clients.All.ReceiveMessage(message);
-        }
+        // public async Task SendMessage(ChatMessage message)
+        // {
+        //     //await Clients.All.SendAsync("ReceiveMessage", message);
+        //     await Clients.All.ReceiveMessage(message);
+        // }
     }
 }
