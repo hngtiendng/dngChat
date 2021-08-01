3676c796b2879774b5b6e7bbc417fd5f61809e28
hngtiendng <hngtiendng@gmail.com>
2021-08-01 12:10:06 +0700

    update chatHub

--- DngChat.API/Hubs/ChatHub.cs
+++ DngChat.API/Hubs/ChatHub.cs
@@ -1,10 +1,11 @@
 namespace DngChat.API.Hubs
 {
-    public class ChatHub:Hub
+    public class ChatHub:Hub<IChatClient>
     {
         public async Task SendMessage(ChatMessage message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", message);
+            //await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.All.ReceiveMessage(message);
         }
     }
 }
--- DngChat.API/Hubs/Clients/IChatClient.cs
+++ DngChat.API/Hubs/Clients/IChatClient.cs
@@ -2,6 +2,6 @@ namespace DngChat.API.Hubs.Clients
 {
     public interface IChatClient
     {
-         Task ReceiveMessage(ChatMessage message);
+        Task ReceiveMessage(ChatMessage message);
     }
 }
